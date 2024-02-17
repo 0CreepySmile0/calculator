74477e92eb74758b7990b5fbfca1a2fcd6cb0032
Yutivich TUNUENG <yutivich2005@gmail.com>
2024-02-17 13:03:27 +0700

    Added history and fix most button

--- calculator_ui.py
+++ calculator_ui.py
@@ -22,6 +22,14 @@ class CalculatorUI(tk.Tk):
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=2)
+        self.rowconfigure(2, weight=2)
+        menubar = tk.Menu(self)
+        self.history = tk.Menu(menubar, tearoff=False)
+        self.history.configure(**font)
+        self.history_expression = []
+        self.history_ans = []
+        menubar.add_cascade(label="History", menu=self.history, **font)
+        self.config(menu=menubar)
         self.display_text = tk.StringVar(value="0")
         self.display_list = []
         self.num_list = list("7894561230.")
@@ -36,13 +44,17 @@ class CalculatorUI(tk.Tk):
                                 textvariable=self.display_text, anchor="se")
         misc_list = ["AC", "(", ")", "DEL", "e", "π", "!", "sqrt", "ln", "log₂", "log₁₀"]
         self.misc_pad = Keypad(self, misc_list, 4)
+        self.misc_pad.re_grid_button("log₁₀", column=2, row=2, columnspan=2, **pad)
         self.misc_pad.configure(**font)
-        self.misc_pad.bind("<Button>",  self.button_handler, list("()eπ!"))
+        self.misc_pad.bind("<Button>",  self.func_handler, list("(eπ"))
         self.misc_pad.bind("<Button>", self.clear_handler, ["AC"])
         self.misc_pad.bind("<Button>", self.del_handler, ["DEL"])
         self.misc_pad.bind("<Button>", self.func_handler, misc_list[-4:])
-        self.num_pad.bind("<Button>", self.button_handler, self.num_list)
-        self.operator_pad.bind("<Button>", self.button_handler, list("+-*/^") + ["mod"])
+        self.misc_pad.bind("<Button>", self.operator_handler, ["!"])
+        self.misc_pad.bind("<Button>", self.num_handler, [")"])
+        self.num_pad.bind("<Button>", self.num_handler, self.num_list[:-1])
+        self.num_pad.bind("<Button>", self.operator_handler, ["."])
+        self.operator_pad.bind("<Button>", self.operator_handler, list("+-*/^") + ["mod"])
         self.operator_pad.bind("<Button>", self.calculate_handler, ["="])
         self.display.grid(column=0, row=0, columnspan=self.winfo_screenwidth(),
                           sticky="nsew")
@@ -76,67 +88,142 @@ class CalculatorUI(tk.Tk):
         user_input = event.widget["text"]
         current = self.display_text.get()
         self.display["fg"] = "yellow"
-        if self.calculated:
+        if self.calculated or current == "0":
             self.calculated = False
-        if current == "0":
-            current = f"{user_input}("
-            self.display_list.append(f"{user_input}(")
-        elif current[-1] in self.operator_list:
-            current += f"{user_input}("
-            self.display_list.append(f"{user_input}(")
+            self.display_list.clear()
+            if user_input in list("(eπ"):
+                current = user_input
+                self.display_list.append(user_input)
+            else:
+                current = f"{user_input}("
+                self.display_list.append(f"{user_input}(")
         else:
-            current += f"*{user_input}("
-            self.display_list.append(f"*{user_input}(")
+            if current[-1] == ".":
+                pass
+            elif current[-1].isnumeric() or current[-1] == "!":
+                if user_input in list("(eπ"):
+                    current += f"*{user_input}"
+                    self.display_list.append(f"*{user_input}")
+                else:
+                    current += f"*{user_input}("
+                    self.display_list.append(f"*{user_input}(")
+            else:
+                if user_input in list("(eπ"):
+                    current += user_input
+                    self.display_list.append(user_input)
+                else:
+                    current += f"*{user_input}("
+                    self.display_list.append(f"*{user_input}(")
         self.display_text.set(current)
 
-    def button_handler(self, event):
+    def num_handler(self, event):
         user_input = event.widget["text"]
         current = self.display_text.get()
         self.display["fg"] = "yellow"
-        if current == "0":
-            if user_input in self.operator_list + [".", "!"]:
-                current += user_input
+        if self.calculated:
+            self.calculated = False
+            self.display_list.clear()
+            current = user_input
+            if not user_input == "0":
                 self.display_list.append(user_input)
-            elif user_input in list("(eπ"):
-                user_input = f"*{user_input}"
-                current += user_input
-                self.display_list.append(user_input)
+        else:
+            if user_input == "0":
+                if current == "0":
+                    current = user_input
+                else:
+                    current += user_input
+                    self.display_list.append(user_input)
             else:
-                current = user_input
-                self.display_list.clear()
+                if current == "0":
+                    current = user_input
+                    self.display_list.append(user_input)
+                elif current[-1] == "!":
+                    current += f"*{user_input}"
+                    self.display_list.append(f"*{user_input}")
+                else:
+                    current += user_input
+                    self.display_list.append(user_input)
+        self.display_text.set(current)
+
+    def operator_handler(self, event):
+        user_input = event.widget["text"]
+        current = self.display_text.get()
+        self.display["fg"] = "yellow"
+        if self.calculated:
+            self.calculated = False
+            current += user_input
+            self.display_list.append(user_input)
+        else:
+            if current[-1] == "!":
+                if user_input in "!.":
+                    pass
+                else:
+                    current += user_input
+                    self.display_list.append(user_input)
+            elif current[-1] in list("+-*/^.") + ["mod"]:
+                pass
+            elif current[-1] == "(":
+                if user_input in list("+-."):
+                    current += user_input
+                    self.display_list.append(user_input)
+                else:
+                    pass
+            else:
+                current += user_input
                 self.display_list.append(user_input)
-        elif float(current) < 0:
-            if self.calculated:
-                if user_input in self.operator_list + [".", "!"]:
-                    current += user_input
-                    self.display_list.append(user_input)
-                elif user_input in list("(eπ"):
-                    user_input = f"*{user_input}"
-                    current += user_input
-                    self.display_list.append(user_input)
+        self.display_text.set(current)
 
     def calculate_handler(self, *args):
         try:
             if self.calculated:
                 pass
             else:
+                self.calculated = True
                 expression = self.display_text.get()
-                expression = replace_syntax(simplify_factorial(clean(expression)))
+                math_expression = clean_zero(expression)
+                expression = replace_syntax(simplify_factorial(clean_zero(expression)))
                 ans = eval(expression)
                 self.display_list.clear()
                 if ans == int(ans):
                     ans = int(ans)
-                    self.display_text.set(str(ans))
-                    self.display_list.extend(list(str(ans)))
+                    ans = str(ans)
                 else:
-                    self.display_text.set(f"{ans:.8g}")
-                    self.display_list.extend(list(f"{ans:.8g}"))
-                self.calculated = True
+                    ans = f"{ans:.10g}"
+                self.display_text.set(ans)
+                self.display_list.extend(list(ans))
+                history = f"{math_expression:<30} {'=':<} {ans}"
+                self.history_expression = [history] + self.history_expression
+                self.history_ans = [math_expression] + self.history_ans
+                if len(self.history_ans) > 5:
+                    self.history_ans.pop(-1)
+                    self.history_expression.pop(-1)
+                self.history.delete(0, len(self.history_ans)-1)
+                for i in range(len(self.history_ans)):
+                    self.history.add_command(label=self.history_expression[i],
+                                             command=lambda x=self.history_ans[i]:
+                                             self.history_handler(x), foreground="blue")
         except:
             self.display["fg"] = "red"
+            self.calculated = False
             pygame.mixer.init()
             pygame.mixer.music.load("illuminati.mp3")
             pygame.mixer.music.play()
+
+    def history_handler(self, history):
+        current = self.display_text.get()
+        result = history
+        if self.calculated:
+            self.calculated = False
+            current = result
+            self.display_list.clear()
+        else:
+            if current == "0":
+                current = result
+                self.display_list.clear()
+            else:
+                current += result
+        self.display_list.append(result)
+        self.display_text.set(current)
 
     def run(self):
         self.mainloop()
@@ -153,7 +240,7 @@ def replace_syntax(expression: str):
     return expression
 
 
-def clean(expression: str):
+def clean_zero(expression: str):
     num = ""
     simplified = ""
     for i in expression+" ":
